c4a964cb502fe81bb73f5d10e3a7982d9e7e0142
coderrick <derrick.duncan@temple.edu>
2015-10-18 11:29:12 -0700

    Added dom outline js lib with hello world example

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -1,3 +1,11 @@
 $(function() {
     console.log('Hello World');
+	var myExampleClickHandler = function (element) { console.log('Clicked element:', element); }
+	var myDomOutline = DomOutline({ onClick: myExampleClickHandler });
+
+	// Start outline:
+	myDomOutline.start();
+
+	// Stop outline (also stopped on escape/backspace/delete keys):
+	myDomOutline.stop();
 });
